c2f7b19959cae5921ddc0eea46f9bf6fc7d90ce6
Joshua Causey <joshuaphillip.causey@cvshealth.com>
2021-08-01 09:55:37 -0500

    solved bubble sort

--- specs/bubble-sort/bubble-sort.test.js
+++ specs/bubble-sort/bubble-sort.test.js
@@ -11,7 +11,19 @@
 */
 
 function bubbleSort(nums) {
-  // code goes here
+  let swapped = true;
+  do{
+    swapped = false
+    for(let i = 0; i < nums.length; i++){
+      if(nums[i] > nums[i + 1]){
+        let temp = nums[i];
+        nums[i] = nums[i + 1];
+        nums[i + 1] = temp;
+        swapped = true;
+      }
+    }
+  } while(swapped)
+  return nums;
 }
 
 // unit tests
